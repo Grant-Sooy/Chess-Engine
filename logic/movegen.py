846d81e6d19f6f2e5f0ffab548bfd1d4142915ce
Grant <gmsooy@cdsdesign.net>
2026-07-08 22:05:05 -0400

    Feat: Finished generating attack baord for all pieces.

--- logic/movegen.py
+++ logic/movegen.py
@@ -1,6 +1,6 @@
 import random
 
-import logic.board as board
+import board as board
 
 #--------------------------------------------------------------------------------
 # PIECE MOVEMENT LOGIC
@@ -278,7 +278,7 @@ def get_rook_attacks(square, occupancy):
 # BISHOP ATTACK BOARDS
 #--------------------------------------------------------------------------------
 
-def bishop_occupancy_squares(square: int) -> int:
+def bishop_occupancy_mask(square: int) -> int:
     attacks = 0
 
     rank = square // 8
@@ -313,3 +313,168 @@ def bishop_occupancy_squares(square: int) -> int:
         fsw -= 1
 
     return attacks & 0xFFFFFFFFFFFFFFFF
+
+BISHOP_MASKS = [bishop_occupancy_mask(sq) for sq in range(64)]
+
+def bishop_attacks_on_the_fly(square, occupancy):
+    attacks = 0
+    rank = square // 8
+    file = square % 8
+
+    # north east
+    rne = rank + 1
+    fne = file + 1
+    while rne <= 7 and fne <= 7:
+        attacks |= (1 << (rne * 8 + fne))
+        if occupancy & (1 << (rne * 8 + fne)):
+            break
+        rne += 1
+        fne += 1
+
+    # north west
+    rnw = rank + 1
+    fnw = file - 1
+    while rnw <= 7 and fnw >= 0:
+        attacks |= (1 << (rnw * 8 + fnw))
+        if occupancy & (1 << (rnw * 8 + fnw)):
+            break
+        rnw += 1
+        fnw -= 1
+
+    # south east
+    rse = rank - 1
+    fse = file + 1
+    while rse >= 0 and fse <= 7:
+        attacks |= (1 << (rse * 8 + fse))
+        if occupancy & (1 << (rse * 8 + fse)):
+            break
+        rse -= 1
+        fse += 1
+
+    # south west
+    rsw = rank - 1
+    fsw = file - 1
+    while rsw >= 0 and fsw >= 0:
+        attacks |= (1 << ( rsw * 8 + fsw))
+        if occupancy & (1 << ( rsw * 8 + fsw)):
+            break
+        rsw -= 1
+        fsw -= 1
+
+    return attacks & 0xFFFFFFFFFFFFFFFF
+
+BISHOP_MAGICS = [
+    0x220080328c080,
+    0x4541842002200,
+    0x4228024401200800,
+    0x804040184490268,
+    0x8024104400200804,
+    0x401032012010100,
+    0x2809010300814,
+    0x800101809008022c,
+    0x400801040e80,
+    0x1000806280200a0,
+    0x80440c1404024000,
+    0x402080841000000,
+    0x2028242420800000,
+    0x20802890200,
+    0x9200050802110410,
+    0x50001200a0841040,
+    0x2040088430040100,
+    0x4020000408008100,
+    0x4040214040208,
+    0x4093200c04048004,
+    0x28040082060a0210,
+    0x80231600900801,
+    0x1800408886101150,
+    0x40c80021080880,
+    0x2026491010101004,
+    0xa212008291c01,
+    0x1008040242016204,
+    0x840040010410021,
+    0x250084000c802004,
+    0x8090028413080100,
+    0xc0808002080404,
+    0xc8c011000405200,
+    0x3203001081104,
+    0x9100841104300,
+    0x160220808010800,
+    0x408601800110105,
+    0x2208020400011010,
+    0x9000c080051000,
+    0x10010205204200,
+    0x40024c0020094200,
+    0x8021006a0246900,
+    0xc040206805804,
+    0xa002428000401,
+    0x8002019000801,
+    0x2600902ce00,
+    0x6840480801410021,
+    0x20a0801000208,
+    0x4011010111000200,
+    0x201420a20218204,
+    0xc1018804423000,
+    0xc014010080902000,
+    0x3820048210440100,
+    0x800801220a20100,
+    0x4250418108b00c2,
+    0x810102108008060,
+    0x20600860888d8002,
+    0x701010718a00400,
+    0x2e00415208140200,
+    0x100122093000,
+    0x2014000020840c00,
+    0x64000228030408,
+    0x20000684080208,
+    0x600100410040250,
+    0x200284100a014502,
+]
+BISHOP_TABLES = [None] * 64
+
+for square in range(64):
+    mask = BISHOP_MASKS[square]
+    N = bin(mask).count('1')
+    num_variations = 1 << N
+    table = [None] * num_variations
+    for index in range(num_variations):
+        occ = generate_occupancy(index, mask)
+        idx = ((occ * BISHOP_MAGICS[square]) & 0xFFFFFFFFFFFFFFFF) >> (64 - N)
+        table[idx] = bishop_attacks_on_the_fly(square, occ)
+    BISHOP_TABLES[square] = table
+
+def get_bishop_attacks(square, occupancy):
+    mask = BISHOP_MASKS[square]
+    N = bin(mask).count('1')
+    index = ((occupancy & mask) * BISHOP_MAGICS[square] & 0xFFFFFFFFFFFFFFFF) >> (64 - N)
+    return BISHOP_TABLES[square][index]
+
+#--------------------------------------------------------------------------------
+# QUEEN ATTACK BOARDS
+#--------------------------------------------------------------------------------
+
+def get_queen_attacks(square, occupancy):
+    return get_rook_attacks(square, occupancy) | get_bishop_attacks(square, occupancy)
+
+#--------------------------------------------------------------------------------
+# PAWN ATTACK BOARDS
+#--------------------------------------------------------------------------------
+
+def generate_pawn_attacks(square, color):
+    attacks = 0
+    bitboard = 1 << square
+
+    if color == board.white:
+        if bitboard & NOT_A_FILE:
+            attacks |= (bitboard >> 7)
+        if bitboard & NOT_H_FILE:
+            attacks |= (bitboard >> 9)
+    else:
+        if bitboard & NOT_A_FILE:
+            attacks |= (bitboard << 9)
+        if bitboard & NOT_H_FILE:
+            attacks |= (bitboard << 7)
+
+    return attacks & 0xFFFFFFFFFFFFFFFF
+
+WHITE_PAWN_ATTACKS = [generate_pawn_attacks(sq, board.white) for sq in range(64)]
+BLACK_PAWN_ATTACKS = [generate_pawn_attacks(sq, board.black) for sq in range(64)]
