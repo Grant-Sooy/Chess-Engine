54435c23301f2c636a1d4ebeff36d5e83a40faf0
Grant <gmsooy@cdsdesign.net>
2026-07-11 23:38:37 -0400

    Feat: added check conditon to moves to prevent illegal moves.

--- logic/moves.py
+++ logic/moves.py
@@ -202,3 +202,91 @@ def pawn_moves(B, color):
                 moves.append((square, dest))
 
     return moves
+
+#--------------------------------------------------------------------------------
+# FURTHER LOGIC
+#--------------------------------------------------------------------------------
+
+def generate_all_moves(B, color):
+    moves = []
+    moves += pawn_moves(B, color)
+    moves += knight_moves(B, color)
+    moves += bishop_moves(B, color)
+    moves += rook_moves(B, color)
+    moves += queen_moves(B, color)
+    moves += king_moves(B, color)
+    return moves
+
+def is_in_check(B, color):
+    if color == board.white:
+        king_square = board.ctz(B[board.Wking])
+
+        enemy_pawns = B[board.Bpawn]
+        enemy_knights = B[board.Bknight]
+        enemy_bishops = B[board.Bbishop]
+        enemy_rooks = B[board.Brook]
+        enemy_queens = B[board.Bqueen]
+        enemy_kings = B[board.Bking]
+        pawn_attacks = atktbls.WHITE_PAWN_ATTACKS
+    else:
+        king_square = board.ctz(B[board.Bking])
+
+        enemy_pawns = B[board.Wpawn]
+        enemy_knights = B[board.Wknight]
+        enemy_bishops = B[board.Wbishop]
+        enemy_rooks = B[board.Wrook]
+        enemy_queens = B[board.Wqueen]
+        enemy_kings = B[board.Wking]
+        pawn_attacks = atktbls.BLACK_PAWN_ATTACKS
+
+
+    if atktbls.KNIGHT_ATTACKS[king_square] & enemy_knights:
+        return True
+    if pawn_attacks[king_square] & enemy_pawns:
+        return True
+    if atktbls.KING_ATTACKS[king_square] & enemy_kings:
+        return True
+    if atktbls.get_bishop_attacks(king_square, B[board.all_boards]) & (enemy_bishops | enemy_queens):
+        return True
+    if atktbls.get_rook_attacks(king_square, B[board.all_boards]) & (enemy_rooks | enemy_queens):
+        return True
+
+    return False
+
+def make_move(B, move):
+    src, dest = move
+    piece = board.get_piece_at(B, src)
+    captured = board.get_piece_at(B, dest)
+
+    B[piece] = board.set_bit(B[piece], dest)
+    B[piece] = board.remove_bit(B[piece], src)
+
+    if captured is not None:
+        B[captured] = board.remove_bit(B[captured], dest)
+
+    board.update_bitboards(B)
+    return captured
+
+def unmake_move(B, move, captured):
+    src, dest = move
+    piece = board.get_piece_at(B, dest)
+
+    B[piece] = board.set_bit(B[piece], src)
+    B[piece] = board.remove_bit(B[piece], dest)
+
+    if captured is not None:
+        B[captured] = board.set_bit(B[captured], dest)
+
+    board.update_bitboards(B)
+
+def generate_legal_moves(B, color):
+    pseudolegal = generate_all_moves(B, color)
+    legal = []
+
+    for move in pseudolegal:
+        captured = make_move(B, move)
+        if not is_in_check(B, color):
+            legal.append(move)
+        unmake_move(B, move, captured)
+
+    return legal
